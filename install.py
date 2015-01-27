1d41a8e7090c2fcc4644c251376d2251a81d3d57
Ștefan Istrate <stefan.istrate@gmail.com>
2015-01-27 18:23:48 -0500

    Improved user interaction at installation time.

--- install.py
+++ install.py
@@ -4,63 +4,77 @@
 import os
 import sys
 
-def make_symlink(dest, dest_dir, src, src_dir):
-    full_dest = '%s/%s' % (dest_dir, dest)
+CWD = os.getcwd()
+HOME = os.environ['HOME']
+COLOR_NONE = '\033[0m'
+COLOR_GREEN = '\033[0;32m'
+COLOR_RED = '\033[0;31m'
+
+def INFO(string):
+    return '%s%s%s' % (COLOR_GREEN, string, COLOR_NONE)
+
+def ERROR(string):
+    return '%s%s%s' % (COLOR_RED, string, COLOR_NONE)
+
+def make_symlink(src, dest_dir, dest):
+    src_dir = CWD
     full_src = '%s/%s' % (src_dir, src)
-    print 'GOAL: %s -> %s' % (full_dest, full_src)
+    full_dest = '%s/%s' % (dest_dir, dest)
+    print 'Source:\t\t%s' % INFO(full_src)
+    print 'Destination:\t%s' % INFO(full_dest)
 
     if not os.path.isdir(dest_dir):
-        print 'INVALID DESTINATION: %s' % dest_dir
-        print 'SKIPPING...\n'
+        print ('Skipping because base destination directory does not exist: %s'
+               % ERROR(dest_dir))
+        print ''
         return
 
     (leaf_dest_dir, _) = os.path.split(full_dest)
     if not os.path.isdir(leaf_dest_dir):
         os.makedirs(leaf_dest_dir)
     if os.path.exists(full_dest):
-        print 'OOOPS! FILE EXISTS: %s' % full_dest
+        print ERROR('Ooops! Destination exists!')
         while True:
-            print 'Should I continue with this symlink? [y/n]'
+            print ERROR('Should I overwrite it? [y/n]'),
             answer = raw_input().lower()
             if answer == 'n':
                 break
             if answer == 'y':
                 os.renames(full_dest, '%s.bak' % full_dest)
-                print 'BACKUP: %s -> %s.bak' % (full_dest, full_dest)
+                print ('Existing destination file backed up at:\t%s'
+                       % INFO('%s.bak' % full_dest))
                 os.symlink(full_src, full_dest)
-                print 'SUCCESS'
+                print 'Done.'
                 break
     else:
         os.symlink(full_src, full_dest)
-        print 'SUCCESS'
+        print 'Done.'
     print ''
 
 
 if __name__ == '__main__':
-    CWD = os.getcwd()
-    HOME = os.environ['HOME']
-    print 'Working directory: %s' % CWD
-    print 'Home directory: %s' % HOME
+    print 'Working directory: %s' % INFO(CWD)
+    print 'Home directory: %s' % INFO(HOME)
     print ''
 
     # Adobe Photoshop.
-    make_symlink('Presets/Scripts/WebSharpener_Settings.xml',
+    make_symlink('adobe-photoshop/WebSharpener_Settings.xml',
                  '/Applications/Adobe Photoshop CC 2014',
-                 'adobe-photoshop/WebSharpener_Settings.xml', CWD)
+                 'Presets/Scripts/WebSharpener_Settings.xml')
 
     # Bash.
-    make_symlink('.bashrc', HOME, 'bash/bashrc', CWD)
-    make_symlink('.dircolors.ansi-dark', HOME, 'bash/dircolors.ansi-dark', CWD)
-    make_symlink('.profile', HOME, 'bash/profile', CWD)
+    make_symlink('bash/bashrc', HOME, '.bashrc')
+    make_symlink('bash/dircolors.ansi-dark', HOME, '.dircolors.ansi-dark')
+    make_symlink('bash/profile', HOME, '.profile')
 
     # Git.
-    make_symlink('.gitconfig', HOME, 'git/gitconfig', CWD)
+    make_symlink('git/gitconfig', HOME, '.gitconfig')
 
     # GNOME Terminal.
     if sys.platform == 'linux2':
-        make_symlink('.gnome-terminal-solarize.sh', HOME,
-                     'gnome-terminal/solarize.sh', CWD)
+        make_symlink('gnome-terminal/solarize.sh',
+                     HOME, '.gnome-terminal-solarize.sh')
 
     # Vim.
-    make_symlink('.vimrc', HOME, 'vim/vimrc', CWD)
-    make_symlink('.vim/after', HOME, 'vim/after', CWD)
+    make_symlink('vim/vimrc', HOME, '.vimrc')
+    make_symlink('vim/after', HOME, '.vim/after')
